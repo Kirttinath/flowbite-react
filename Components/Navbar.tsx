7c252d957606f75bddd9621e406fb9a9d3b3f32c
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-23 00:32:15 +0530

    Added the images

---
+++ Components/Navbar.tsx
@@ -0,0 +1,106 @@
+"use client";
+import styled from "styled-components";
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <Navleft>
+        <Logo>
+          <Imagecomp src={logoImage} alt="Your Company Logo" />
+          <Title href="#">Flowbite</Title>
+        </Logo>
+        <Links>
+          <NavLink href="#">Company</NavLink>
+          <NavLink href="#">MarketPlace</NavLink>
+          <NavLink href="#">Features</NavLink>
+          <NavLink href="#">Team</NavLink>
+          <NavLink href="#">Contact</NavLink>
+        </Links>
+      </Navleft>
+      <NavRight>
+        <Button1>Log In</Button1>
+        <Button2>Get Started</Button2>
+      </NavRight>
+    </Nav>
+  );
+};
+
+const Nav = styled.nav`
+  display: flex;
+  height: 10vh;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0em 5em;
+
+  background-color: #ffffff;
+`;
+
+const Navleft = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+`;
+const Logo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5em;
+`;
+const Imagecomp = styled(Image)`
+  width: 2em;
+`;
+const Title = styled(Link)`
+  font-weight: bold;
+  font-size: 1.5em;
+`;
+const Links = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media screen and(min-width:280px) and (max-width: 1000px) {
+    display: none;
+  }
+`;
+const NavLink = styled.a`
+  margin: 0 10px;
+  text-decoration: none;
+  color: #333;
+  font-size: 16px;
+  font-weight: 500;
+
+  &:hover {
+    color: #007bff;
+    transform: 0.2s ease-in-out;
+  }
+`;
+const NavRight = styled.div`
+  display: flex;
+  padding: var(--25, 10px) var(--5, 20px);
+  justify-content: center;
+  align-items: center;
+  gap: var(--2, 8px);
+`;
+const Button1 = styled.button`
+  border-radius: var(--rounded-lg, 8px);
+  background: var(--primary-700, white);
+  color: var(--white, black);
+
+  font-size: 14px;
+  padding: var(--25, 10px) var(--5, 20px);
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%;
+`;
+const Button2 = styled.button`
+  border-radius: var(--rounded-lg, 8px);
+  background: var(--primary-700, #1a56db);
+  color: var(--white, #fff);
+
+  font-size: 14px;
+  padding: var(--25, 10px) var(--5, 20px);
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%;
+`;
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+import styled from "styled-components";
+import Homepage from "../Components/Homepage";
+
 export default function Home() {
-  return <>Hii</>;
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
 }
+
+const main = styled.div`
+  display: flex;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+`;
