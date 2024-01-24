c5407854984a3d4a8b37f2d8e47adb847afe581b
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-25 03:44:07 +0530

    tailwind css added

--- pages/Signin.tsx
+++ pages/Signin.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { Button } from "flowbite-react";
+import styled from "styled-components";
 import { signOut } from "next-auth/react";
 type Props = {};
 
@@ -15,7 +16,7 @@ const Signin = ({}: Props) => {
 
   return (
     <>
-      <div>
+      <div className="text-25 font-bold text-green-500">
         <div>{session?.data?.user?.name}</div>
         <br />
         <div>{session?.data?.user?.email}</div>
